9642380d21af4a84c7a8de0cdd8185e8b934413c
antonio-st@yandex.ru <antonio-st@yandex.ru>
2023-06-19 17:56:11 +0300

    add db filling script :bash/run_init_db.sh

--- bash/run_init_db.sh
+++ bash/run_init_db.sh
@@ -1,1 +1,51 @@
 #!/bin/bash
+
+echo -e "\n$ Скачиваем Docker образ PostgreSQL \n"
+
+docker pull postgres:14.8
+
+echo -e "\n Done \n"
+
+sleep 5
+
+echo -e "\n$ Запускаем контейнер PostgreSQL \n"
+
+
+docker run --name sde_test_db \
+--rm \
+-e POSTGRES_PASSWORD="@sde_password012" \
+-e POSTGRES_USER="test_sde" \
+-e POSTGRES_DB="demo" \
+-e PGDATA=/var/lib/postgresql/data/pgdata \
+-v $(pwd)/sql:/var/lib/postgresql/data \
+-p 5434:5434 \
+-d \
+postgres
+
+echo -e "\n$ Контейнер с PostgreSQL запущен\n"
+
+
+sleep 1
+
+echo -e "\n$ Запускаем контейнер для заполнения БД, немного подождите....\n"
+
+
+docker exec sde_test_db \
+psql -U \
+test_sde \
+-d demo \
+-f /var/lib/postgresql/data/init_db/demo.sql
+sleep 20
+
+
+echo -e "\n$ Проверим поднятый контейнер\n"
+
+docker ps
+
+echo -e "\n$ Проверим кол-во записей в таблице bookings, должно быть 262788 \n"
+
+docker exec sde_test_db psql -U test_sde -d demo -c "SELECT COUNT(*) FROM bookings.bookings"
+sleep 5
+
+
+echo -e "\n$ Done...\n"
